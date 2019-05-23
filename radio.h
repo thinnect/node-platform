a1b54692cb5fb391769e318a476b1ffe46e25314
Veiko Rütter <veiko@thinnect.com>
2019-05-23 12:31:57 +0300

    added radio_reenable_channel_panid

--- radio.h
+++ radio.h
@@ -21,6 +21,7 @@ typedef struct radio_callback_struct radio_callback_t;
 comms_layer_t* radio_init(uint16_t channel, uint16_t pan_id, uint16_t address);
 void radio_idle();
 void radio_reenable();
+void radio_reenable_channel_panid(uint16_t channel, uint16_t pan_id);
 void radio_poll(void);
 int radio_recv_register(uint8_t am_id, void *user, void (*recv)(void *user, const void *buf, int count, uint16_t src, uint16_t dst, uint8_t am_group, uint8_t am_id, int rssi));
 // int radio_send(const void *buf, int count, uint16_t dst, uint8_t am_group, uint8_t am_id);
