eb45b15bc1f753e01096eb63997ae32f190c4aca
Veiko Rütter <veiko@thinnect.com>
2019-06-19 10:01:40 +0300

    added multiple spi cs support

--- retargetspi.c
+++ retargetspi.c
@@ -9,7 +9,6 @@
 #include "retargetspi.h"
 #include "retargetspiconfig.h"
 
-#include "spi.h"
 #include <stdio.h>
 #include "em_cmu.h"
 #include "em_gpio.h"
@@ -23,6 +22,12 @@ void RETARGET_SpiInit() {
 
 	CMU_ClockEnable(cmuClock_GPIO, true);
 	GPIO_PinModeSet(RETARGET_SPI_CS_PORT, RETARGET_SPI_CS_PIN, gpioModePushPull, 1);
+#ifdef RETARGET_SPI_CS1_PORT
+	GPIO_PinModeSet(RETARGET_SPI_CS1_PORT, RETARGET_SPI_CS1_PIN, gpioModePushPull, 1);
+#endif
+#ifdef RETARGET_SPI_CS2_PORT
+	GPIO_PinModeSet(RETARGET_SPI_CS2_PORT, RETARGET_SPI_CS2_PIN, gpioModePushPull, 1);
+#endif
 	GPIO_PinModeSet(RETARGET_SPI_MISO_PORT, RETARGET_SPI_MISO_PIN, gpioModeInput, 1);
 	GPIO_PinModeSet(RETARGET_SPI_MOSI_PORT, RETARGET_SPI_MOSI_PIN, gpioModePushPull, 1);
 	GPIO_PinModeSet(RETARGET_SPI_SCK_PORT, RETARGET_SPI_SCK_PIN, gpioModePushPull, 1);
@@ -68,26 +73,43 @@ void RETARGET_SpiDisable() {
 	//GPIO_PinModeSet(RETARGET_SPI_CS_PORT, RETARGET_SPI_CS_PIN, gpioModeDisabled, 0);
 }
 
-void spi_cs(uint8_t active) {
-	if(active) {
+void spi_cs(int cs, uint8_t active) {
+	if((cs == 0) && active) {
 		GPIO_PinOutClear(RETARGET_SPI_CS_PORT, RETARGET_SPI_CS_PIN);
 	}else{
 		GPIO_PinOutSet(RETARGET_SPI_CS_PORT, RETARGET_SPI_CS_PIN);
 	}
+#ifdef RETARGET_SPI_CS1_PORT
+	if((cs == 1) && active) {
+		GPIO_PinOutClear(RETARGET_SPI_CS1_PORT, RETARGET_SPI_CS1_PIN);
+	}else{
+		GPIO_PinOutSet(RETARGET_SPI_CS1_PORT, RETARGET_SPI_CS1_PIN);
+	}
+#endif
+#ifdef RETARGET_SPI_CS2_PORT
+	if((cs == 2) && active) {
+		GPIO_PinOutClear(RETARGET_SPI_CS2_PORT, RETARGET_SPI_CS2_PIN);
+	}else{
+		GPIO_PinOutSet(RETARGET_SPI_CS2_PORT, RETARGET_SPI_CS2_PIN);
+	}
+#endif
 }
 
 int RETARGET_SpiTransfer(int cs, const void *out, void *in, int32_t len) {
+	if(len < 1)return(len);
+	spi_cs(cs, 1);
 	for(int32_t i = 0; i < len; i++) {
 		((uint8_t *)in)[i] = USART_SpiTransfer(RETARGET_SPI_UART, ((uint8_t *)out)[i]);
 	}
+	spi_cs(cs, 0);
 	return 0;
 }
 
 int RETARGET_SpiTransferHalf(int cs, const void *out, int32_t out_len, void *in, int32_t in_len) {
 	int32_t i;
-	if((cs != 0) || (out_len < 0) || (in_len < 0))return(-1);
+	if((out_len < 0) || (in_len < 0))return(-1);
 	RETARGET_SPI_UART->CMD = USART_CMD_CLEARTX | USART_CMD_CLEARRX;
-	spi_cs(1);
+	spi_cs(cs, 1);
 	if(out_len) {
 		RETARGET_SPI_UART->CMD = USART_CMD_TXTRIDIS;
 		for(i = 0; i < out_len; i++) {
@@ -98,6 +120,6 @@ int RETARGET_SpiTransferHalf(int cs, const void *out, int32_t out_len, void *in,
 	for(i = 0; i < in_len; i++) {
 		((uint8_t *)in)[i] = USART_SpiTransfer(RETARGET_SPI_UART, 0xFF);
 	}
-	spi_cs(0);
+	spi_cs(cs, 0);
 	return(0);
 }
