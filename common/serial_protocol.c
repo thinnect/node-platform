ec9bc850a3874e9b6bb08df7272b2725d4244eb7
Raido Pahtma <raidopahtma@gmail.com>
2019-09-13 12:48:15 +0300

    Implement parts of serial_protocol.

--- common/serial_protocol.c
+++ common/serial_protocol.c
@@ -1,7 +1,7 @@
 /*
  * TinyOS Serial packet protocol implementation on top of serial_hdlc.
  *
- * The serial protocol covers acknowledged packets and their acks and
+ * The serial protocol covers acknowledged packets, their acks and
  * packets that do not expect an acknowledgement.
  *
  * Copyright Thinnect Inc. 2019
@@ -10,41 +10,13 @@
  */
 
 #include "serial_protocol.h"
+#include "serial_protocol_packets.h"
 
 #include "serial_hdlc.h"
 
+#include "cmsis_os2.h"
+
 #include <string.h>
-
-#define SERIAL_PROTOCOL_PACKET    0x45
-#define SERIAL_PROTOCOL_ACKPACKET 0x44
-#define SERIAL_PROTOCOL_ACK       0x43
-
-#pragma pack(push, 1)
-typedef struct serial_protocol_ack
-{
-    uint8_t protocol; // SERIAL_PROTOCOL_ACK - 0x43
-    uint8_t seq_num;
-    // uint8_t crc[2]; - handled by lower layer
-} serial_protocol_ack_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct serial_protocol_ackpacket
-{
-    uint8_t protocol; // SERIAL_PROTOCOL_ACKPACKET - 0x44
-    uint8_t seq_num;
-    uint8_t payload[]; // First byte of the "payload" would be the dispatcher
-    // uint8_t crc[2]; - handled by lower layer
-} serial_protocol_ackpacket_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct serial_protocol_packet
-{
-    uint8_t protocol; // SERIAL_PROTOCOL_PACKET - 0x45
-    uint8_t payload[]; // First byte of the "payload" would be the dispatcher
-} serial_protocol_packet_t;
-#pragma pack(pop)
 
 #include "loglevels.h"
 #define __MODUUL__ "serp"
@@ -55,18 +27,49 @@ static bool m_tx_ack_wait;
 static uint8_t m_tx_seq_num;
 static uint8_t m_rx_seq_num;
 
-static serial_receive_f * mp_receiver;
+// Data packet being sent, NULL if idle
+static const uint8_t* m_data;
+static uint8_t m_data_length;
 
-void serial_protocol_init (serial_receive_f* receiver)
+static osMutexId_t m_sp_mutex;
+static osTimerId_t m_ack_timeout_timer;
+
+static serial_receive_f * mp_receiver;
+static serial_send_done_f * mp_senddone;
+
+static void serial_protocol_ack_timeout_cb(void* argument);
+
+void serial_protocol_init (serial_receive_f* rcvr, serial_send_done_f * sdf)
 {
+    m_data = NULL;
+    m_data_length = 0;
+
     m_tx_ack_wait = false;
     m_tx_seq_num = 0;
     m_rx_seq_num = 0;
-    mp_receiver = receiver;
+    mp_receiver = rcvr;
+    mp_senddone = sdf;
+
+    m_sp_mutex = osMutexNew(NULL);
+    m_ack_timeout_timer = osTimerNew(&serial_protocol_ack_timeout_cb, osTimerOnce, NULL, NULL);
+}
+
+static void serial_protocol_ack_timeout_cb(void* argument)
+{
+    osMutexAcquire(m_sp_mutex, osWaitForever);
+    if (NULL != m_data)
+    {
+        mp_senddone(m_data, m_data_length, false);
+        m_data = NULL;
+        m_tx_ack_wait = false;
+    }
+    osMutexRelease(m_sp_mutex);
 }
 
 void serial_protocol_receive (const uint8_t data[], uint8_t length)
 {
+    osMutexAcquire(m_sp_mutex, osWaitForever);
+
     if (length > 0)
     {
         uint8_t protocol = data[0];
@@ -80,9 +83,11 @@ void serial_protocol_receive (const uint8_t data[], uint8_t length)
                     {
                         if(ack->seq_num == m_tx_seq_num)
                         {
-                            m_tx_ack_wait = false;
                             debug1("ack %02X", (unsigned int)ack->seq_num);
-                            // TODO signal send-done?
+                            osTimerStop(m_ack_timeout_timer);
+                            mp_senddone(m_data, m_data_length, true);
+                            m_data = NULL;
+                            m_tx_ack_wait = false;
                         }
                         else
                         {
@@ -151,25 +156,49 @@ void serial_protocol_receive (const uint8_t data[], uint8_t length)
             break;
         }
     }
+
+    osMutexRelease(m_sp_mutex);
 }
 
-bool serial_protocol_send (const uint8_t data[], uint8_t length)
+bool serial_protocol_send (const uint8_t data[], uint8_t length, bool ack)
 {
-    uint8_t payload[length + sizeof(serial_protocol_packet_t)];
-    serial_protocol_packet_t* packet = (serial_protocol_packet_t*)payload;
-    packet->protocol = SERIAL_PROTOCOL_PACKET;
-    memcpy(packet->payload, data, length);
-    serial_hdlc_send(payload, sizeof(payload));
-    return true;
-}
+    bool ret = true;
 
-int16_t serial_protocol_send_ackpacket (const uint8_t data[], uint8_t length)
-{
-    uint8_t payload[length + sizeof(serial_protocol_packet_t)];
-    serial_protocol_ackpacket_t* packet = (serial_protocol_ackpacket_t*)payload;
-    packet->protocol = SERIAL_PROTOCOL_ACKPACKET;
-    packet->seq_num = ++m_tx_seq_num;
-    memcpy(packet->payload, data, length);
-    serial_hdlc_send(payload, sizeof(payload));
-    return m_tx_seq_num;
+    osMutexAcquire(m_sp_mutex, osWaitForever);
+
+    if (NULL != m_data)
+    {
+        ret = false;
+    }
+    else if (ack)
+    {
+        uint8_t payload[length + sizeof(serial_protocol_packet_t)];
+        serial_protocol_ackpacket_t* packet = (serial_protocol_ackpacket_t*)payload;
+        packet->protocol = SERIAL_PROTOCOL_ACKPACKET;
+        packet->seq_num = ++m_tx_seq_num;
+        memcpy(packet->payload, data, length);
+        serial_hdlc_send(payload, sizeof(payload));
+
+        m_data = data;
+        m_data_length = length;
+        m_tx_ack_wait = true;
+        osTimerStart(m_ack_timeout_timer, 100UL);
+    }
+    else
+    {
+        uint8_t payload[length + sizeof(serial_protocol_packet_t)];
+        serial_protocol_packet_t* packet = (serial_protocol_packet_t*)payload;
+        packet->protocol = SERIAL_PROTOCOL_PACKET;
+        memcpy(packet->payload, data, length);
+        serial_hdlc_send(payload, sizeof(payload));
+
+        m_data = data;
+        m_data_length = length;
+        m_tx_ack_wait = false;
+        osTimerStart(m_ack_timeout_timer, 1UL); // Minimal possible delay
+    }
+
+    osMutexRelease(m_sp_mutex);
+
+    return ret;
 }
--- include/serial_protocol.h
+++ include/serial_protocol.h
@@ -1,30 +1,56 @@
-#pragma once
+/**
+ * SerialProtocol layer API.
+ *
+ * Copyright Thinnect Inc. 2019
+ * @license MIT
+ * @author Raido Pahtma
+ */
+#ifndef SERIAL_PROTOCOL_H_
+#define SERIAL_PROTOCOL_H_
 
 #include <stdint.h>
 #include <stdbool.h>
 
+/**
+ * Receive callback.
+ * @param data - Received packet payload.
+ * @param length - Received packet payload length.
+ * @return True, if packet accepted for processing, False if dropped.
+ */
 typedef bool serial_receive_f (const uint8_t data[], uint8_t length);
-typedef void serial_send_done_f (const uint8_t data[], uint8_t length);
 
-void serial_protocol_init (serial_receive_f * receiver);
+/**
+ * SendDone callback.
+ * @param data - Sent packet payload.
+ * @param length - Sent packet payload length.
+ * @param acked - True if ack requested and received.
+ */
+typedef void serial_send_done_f (const uint8_t data[], uint8_t length, bool acked);
 
+/**
+ * Initialize the serial-protocol layer.
+ *
+ * @param rcvr - Receive callback.
+ * @param sdf - SendDone callback function.
+ */
+void serial_protocol_init (serial_receive_f * rcvr, serial_send_done_f * sdf);
+
+/**
+ * Send a serial-protocol packet.
+ *
+ * @param data - Packet payload.
+ * @param length - Packet payload length.
+ * @param ack - wait for ack.
+ * @return True, if the sdf function provided in init will be called in the future.
+ */
+bool serial_protocol_send (const uint8_t data[], uint8_t length, bool ack);
+
+/**
+ * SerialProtocol layer receive - insert packets received by lower layer.
+ *
+ * @param data - Packet payload.
+ * @param length - Packet payload length.
+ */
 void serial_protocol_receive (const uint8_t data[], uint8_t length);
 
-/**
- * Send a regular serial packet.
- * @param data - Packet payload.
- * @param length - Packet payload length.
- * @return True, if packet sent.
- */
-bool serial_protocol_send (const uint8_t data[], uint8_t length);
-
-/**
- * Send a serial packet and request an acknowledgement.
- * @param data - Packet payload.
- * @param length - Packet payload length.
- * @return < 0 for error, sequence number otherwise (0-255).
- */
-int16_t serial_protocol_send_ackpacket (const uint8_t data[], uint8_t length);
-
-
-void serial_protocol_send_done (uint8_t seq, bool acked);
+#endif//SERIAL_PROTOCOL_H_
