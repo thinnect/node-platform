4d3270e9e87266ae8f8ce768fc70f142b38a1e83
Raido Pahtma <raidopahtma@gmail.com>
2020-01-24 14:09:06 +0200

    Add eui64.h stub.

---
+++ include/eui64.h
@@ -0,0 +1,14 @@
+/**
+ * EUI-64 definitions header.
+ *
+ * Copyright Thinnect Inc. 2020
+ * @license MIT
+ */
+#ifndef EUI64_H_
+#define EUI64_H_
+
+#define EUI64_LENGTH 8
+
+// TODO Must add some actual data structures!
+
+#endif// EUI64_H_
