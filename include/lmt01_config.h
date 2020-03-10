6929581a255b9dedc8aba9da9c5cd16737a1b8a8
Veiko Rütter <veiko@thinnect.com>
2020-03-10 15:41:35 +0200

    configuration support. fixed initializing

---
+++ include/lmt01_config.h
@@ -0,0 +1,20 @@
+#ifndef _LMT01_CONFIG_H_
+#define _LMT01_CONFIG_H_
+
+// ACMP
+#define LMT01_ACMP_DEV_INDEX                 0
+#define LMT01_ACMP_INPUT_PORT                B
+#define LMT01_ACMP_INPUT_PIN                 0
+#define LMT01_ACMP_INPUT_BUSALLOC_INDEX      0
+
+// COUNTER TIMER
+#define LMT01_COUNTER_TIMER_INDEX            2
+
+// ONESHOT TIMER
+#define LMT01_ONESHOT_TIMER_PERIPHERAL_CLOCK 38400000LU
+#define LMT01_ONESHOT_TIMER_INDEX            1
+#define LMT01_ONESHOT_TIMER_PORT             A
+#define LMT01_ONESHOT_TIMER_PIN              6
+
+#endif
+
--- sensors/lmt01.h
+++ sensors/lmt01.h
@@ -10,29 +10,6 @@
 
 #include <stdint.h>
 
-#define LMT01_ACMP_DEV                       ACMP0
-#define LMT01_ACMP_CLOCK                     cmuClock_ACMP0
-#define LMT01_ACMP_IRQ                       ACMP0_IRQn
-#define LMT01_ACMP_INPUT                     acmpInputPB0
-#define LMT01_ACMP_INPUT_PORT                gpioPortB
-#define LMT01_ACMP_INPUT_PIN                 0
-#define LMT01_ACMP_INPUT_BUSALLOC            (GPIO->BBUSALLOC)
-#define LMT01_ACMP_INPUT_BUSALLOC_STATE      GPIO_BBUSALLOC_BEVEN0_ACMP0
-#define LMT01_ACMP_PRS_OUT                   prsSignalACMP0_OUT
-
-#define LMT01_COUNTER_TIMER_DEV              TIMER2
-#define LMT01_COUNTER_TIMER_CLOCK            cmuClock_TIMER2
-#define LMT01_COUNTER_TIMER_IRQ              TIMER2_IRQn
-#define LMT01_COUNTER_TIMER_PRS_CC1          prsConsumerTIMER2_CC1
-
-#define LMT01_ONESHOT_TIMER_DEV              TIMER1
-#define LMT01_ONESHOT_TIMER_CLOCK            cmuClock_TIMER1
-#define LMT01_ONESHOT_TIMER_IRQ              TIMER1_IRQn
-#define LMT01_ONESHOT_TIMER_INDEX            1
-#define LMT01_ONESHOT_TIMER_PERIPHERAL_CLOCK 38400000LU
-#define LMT01_ONESHOT_TIMER_PORT             gpioPortA
-#define LMT01_ONESHOT_TIMER_PIN              6
-
 /**
  * Initializes LMT01 driver
  */
