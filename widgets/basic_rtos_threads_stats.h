2ceb218fa376a99062dfe27bd3f18747cd17ca9a
Konstantin Bilozor <konstantin@thinnect.com>
2020-11-16 16:58:42 +0200

    Added widget to monitor threads.

---
+++ widgets/basic_rtos_threads_stats.h
@@ -0,0 +1,12 @@
+/**
+ * Gather and print RTOS threads statistics.
+ *
+ * Copyright Thinnect Inc. 2020
+ * @license MIT
+ */
+#ifndef BASIC_RTOS_THREADS_STATS_H_
+#define BASIC_RTOS_THREADS_STATS_H_
+
+void basic_rtos_threads_stats();
+
+#endif//BASIC_RTOS_THREADS_STATS_H_
