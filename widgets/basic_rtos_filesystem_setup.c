476b5189058f75c5648f0987937ea425ec57a997
Madis Uusjärv <madis@thinnect.com>
2023-10-19 17:37:45 +0300

    [CORE-416] Read flash size from JEDEC

--- widgets/basic_rtos_filesystem_setup.c
+++ widgets/basic_rtos_filesystem_setup.c
@@ -40,9 +40,13 @@ void basic_rtos_filesystem_setup ()
     spi_flash_init();
 
     // Get dataflash chip ID
-    uint8_t jedec[4] = {0};
-    RETARGET_SpiTransferHalf(0, "\x9F", 1, jedec, 4);
-    info1("jedec %02x%02x%02x%02x", jedec[0], jedec[1], jedec[2], jedec[3]);
+    uint8_t jedec[3] = {0};
+
+    RETARGET_SpiTransferHalf(0, "\x9F", 1, jedec, 3);
+    // info1("JEDEC %02X%02X%02X", jedec[0], jedec[1], jedec[2]);
+    info1("ManfID: %02X", jedec[0]);
+    info1("MemType: %02X", jedec[1]);
+    info1("MemSize: %u bytes", 1 << jedec[2]);
 
     if ((0x00 == jedec[0])||(0xFF == jedec[0])) // Invalid ID, flash not responsive
     {
@@ -54,6 +58,9 @@ void basic_rtos_filesystem_setup ()
         PLATFORM_HardReset(); // A hard-reset may help on some boards
     }
 
+    watchdog_feed();
+
+    // debug1("Mass erase!");
     // spi_flash_mass_erase();
 
     m_spi_fs_driver.read = spi_flash_read;
@@ -64,6 +71,9 @@ void basic_rtos_filesystem_setup ()
     m_spi_fs_driver.suspend = spi_flash_suspend;
     m_spi_fs_driver.lock = spi_flash_lock;
     m_spi_fs_driver.unlock = spi_flash_unlock;
+    // JEDEC ID contains mem size in form 2^N and we need to reserve 1MB for filesystem
+    m_spi_fs_driver.static_size = (1 << jedec[2]) - (1024UL * 1024UL);
+    debug1("size: %u", m_spi_fs_driver.static_size);
 
     // TODO Non-magic numbers SPIFFS is partition 2 (third partition) and filesystem 0?
     fs_init(0, 2, &m_spi_fs_driver);
